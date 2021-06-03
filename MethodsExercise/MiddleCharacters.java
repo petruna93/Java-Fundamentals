bebaec62d525a6023e463e50c062da7b65e133ed
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-06-03 22:58:24 +0300

    Middle Characters

--- MethodsExercise/MiddleCharacters.java
+++ MethodsExercise/MiddleCharacters.java
@@ -1,4 +1,24 @@
-package MethodsExercise;
-
 public class MiddleCharacters {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String result = middleChar(input);
+        System.out.println(result);
+    }
+
+    private static String middleChar(String input) {
+        String result = "";
+
+        if (input.length()%2==0){
+            for (int i = (input.length()/2)-1; i <=(input.length()/2) ; i++) {
+                result=result+input.charAt(i);
+            }
+        }else {
+            for (int i = input.length()/2; i <=input.length()/2 ; i++) {
+                result = result+input.charAt(i);
+            }
+        }
+        return result;
+  
