09aea40f187717678f14ad23cb3e760258b232d5
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-06-03 23:23:25 +0300

    Factorial Division

--- MethodsExercise/FactorialDivision.java
+++ MethodsExercise/FactorialDivision.java
@@ -1,4 +1,25 @@
 package MethodsExercise;
 
+import java.util.Scanner;
+
 public class FactorialDivision {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int first = Integer.parseInt(scanner.nextLine());
+        int second = Integer.parseInt(scanner.nextLine());
+
+        long firstResult = factorial(first);
+        long secondResult = factorial(second);
+
+        double finalResult = (double)firstResult/secondResult;
+        System.out.printf("%.2f",finalResult);
+    }
+
+    private static long factorial(int n) {
+        long sum = 1;
+        for (int i = 1; i <=n ; i++) {
+            sum*=i;
+        }
+        return sum;
+    }
 }
