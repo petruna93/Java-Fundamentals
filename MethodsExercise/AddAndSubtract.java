5b145c711038053c8882376e72f168dea5a1623d
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-06-03 22:35:08 +0300

    Add and Subtract

--- MethodsExercise/AddAndSubtract.java
+++ MethodsExercise/AddAndSubtract.java
@@ -1,4 +1,27 @@
 package MethodsExercise;
 
+import java.util.Scanner;
+
 public class AddAndSubtract {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int first = Integer.parseInt(scanner.nextLine());
+        int second = Integer.parseInt(scanner.nextLine());
+        int third = Integer.parseInt(scanner.nextLine());
+        
+        int sum = sumMethod(first,second);
+        int subtract = subtractMethod(sum,third);
+
+        System.out.println(subtract);
+    }
+
+    private static int subtractMethod(int sum, int third) {
+        int result = sum - third;
+        return result;
+    }
+
+    private static int sumMethod(int first, int second) {
+        int result = first+second;
+        return result;
+    }
 }
