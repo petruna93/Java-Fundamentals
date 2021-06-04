fd5e6729f5f4e2490704aa42889c448e03a70b0e
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-06-04 21:39:39 +0300

    Palindrome Integers

--- MethodsExercise/PalindromeIntegers.java
+++ MethodsExercise/PalindromeIntegers.java
@@ -1,4 +1,25 @@
 package MethodsExercise;
 
+import java.util.Scanner;
+
 public class PalindromeIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("END")){
+            checking(input);
+            input =scanner.nextLine();
+        }
+    }
+
+    private static void checking(String input) {
+        String reverse = new StringBuffer(input).reverse().toString();
+        if (input.equals(reverse)){
+            System.out.println("true");
+        }else {
+            System.out.println("false");
+        }
+
+    }
 }
