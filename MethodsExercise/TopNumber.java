5c612a7790df577a170f8b812bc64a57d2f7899b
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-06-04 22:17:00 +0300

    Top Number

--- MethodsExercise/TopNumber.java
+++ MethodsExercise/TopNumber.java
@@ -1,4 +1,33 @@
 package MethodsExercise;
 
+import java.util.Scanner;
+
 public class TopNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+            int num = i;
+            checked(num);
+        }
+    }
+
+    private static void checked(int num) {
+        int number = num;
+        int count = 0;
+        int sum = 0;
+
+        while (number>0){
+            int lastDigit = number%10;
+            sum+=lastDigit;
+            if (lastDigit%2!=0){
+                count++;
+            }
+            number=number/10;
+        }
+        if (sum%8==0 && count>0){
+            System.out.println(num);
+        }
+    }
 }
