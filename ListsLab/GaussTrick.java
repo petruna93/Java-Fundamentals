2c0dbd7118486bbf5857b0712b410b819e5754fa
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-06-09 23:16:25 +0300

    Gauss' Trick

--- ListsLab/GaussTrick.java
+++ ListsLab/GaussTrick.java
@@ -1,4 +1,27 @@
 package ListsLab;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class GaussTrick {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        int size = numbers.size();
+
+        for (int i = 0; i <size/2 ; i++) {
+            int first = numbers.get(i);
+            int last = numbers.get(numbers.size()-1);
+            numbers.set(i,first+last);
+            numbers.remove(numbers.size()-1);
+        }
+        for (int number:numbers) {
+            System.out.print(number + " ");
+
+        }
+    }
 }
