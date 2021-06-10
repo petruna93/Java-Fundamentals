3b9d54c926de6b62356cfdf4c4185d2e96b666c8
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-06-10 22:49:22 +0300

    List Manipulation Basics

--- ListsLab/ListManipulationBasics.java
+++ ListsLab/ListManipulationBasics.java
@@ -1,4 +1,41 @@
 package ListsLab;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class ListManipulationBasics {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("end")) {
+            String[] command = input.split("\\s++");
+            switch (command[0]) {
+                case "Add":
+                    numbers.add(Integer.parseInt(command[1]));
+                    break;
+                case "Remove":
+                    int indexNum = numbers.indexOf(Integer.parseInt(command[1]));
+                    numbers.remove(indexNum);
+                    break;
+                case "RemoveAt":
+                    numbers.remove(Integer.parseInt(command[1]));
+                    break;
+                case "Insert":
+                    numbers.add(Integer.parseInt(command[2]), Integer.parseInt(command[1]));
+                    break;
+            }
+            input = scanner.nextLine();
+        }
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i) + " ");
+
+        }
+    }
 }
