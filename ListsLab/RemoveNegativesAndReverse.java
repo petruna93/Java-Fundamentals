ea06639a46a5f61e2a7ca8f367bf3b2f6ebab02d
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-06-11 23:11:49 +0300

    Remove Negatives and Reverse

--- ListsLab/RemoveNegativesAndReverse.java
+++ ListsLab/RemoveNegativesAndReverse.java
@@ -1,4 +1,30 @@
-package ListsLab;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class RemoveNegativesAndReverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        for (int i = 0; i <numbers.size() ; i++) {
+            int num = numbers.get(i);
+            if (num<0){
+                numbers.remove(numbers.get(i));
+                i--;
+            }
+        }
+        if (numbers.size()>0){
+        Collections.reverse(numbers);
+        for (int i = 0; i <numbers.size() ; i++) {
+            System.out.print(numbers.get(i) + " ");
+        }
+        }else {
+            System.out.println("empty");
+        }
+    }
 }
