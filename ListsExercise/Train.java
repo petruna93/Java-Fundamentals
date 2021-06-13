480c8324e14c2f74fc0cc53c279c145ec3945e7a
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-06-13 21:42:50 +0300

    Train

--- ListsExercise/Train.java
+++ ListsExercise/Train.java
@@ -1,4 +1,41 @@
 package ListsExercise;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Train {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> wagons = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        int maxCapacity = Integer.parseInt(scanner.nextLine());
+
+        String input = scanner.nextLine();
+         while (!input.equals("end")){
+             String[]tokens = input.split("\\s+");
+             if (tokens[0].equals("Add")){
+                 wagons.add(Integer.parseInt(tokens[1]));
+             }else {
+                 int passengers = Integer.parseInt(tokens[0]);
+                 for (int i = 0; i <wagons.size() ; i++) {
+                     int total = passengers+wagons.get(i);
+                     if (total<=maxCapacity){
+                         wagons.set(i,total);
+                         break;
+                     }else {
+                         continue;
+                     }
+                 }
+             }
+             input=scanner.nextLine();
+         }
+
+        for (int i = 0; i <wagons.size() ; i++) {
+            System.out.print(wagons.get(i) + " ");
+        }
+    }
 }
