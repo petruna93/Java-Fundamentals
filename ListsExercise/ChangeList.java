2f3cc2f7c0f2fc22028832639a46953a7624150d
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-06-13 22:15:05 +0300

    Change List

--- ListsExercise/ChangeList.java
+++ ListsExercise/ChangeList.java
@@ -1,4 +1,45 @@
 package ListsExercise;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class ChangeList {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("end")){
+            String[]tokens = input.split("\\s+");
+            String command = tokens[0];
+            int element = Integer.parseInt(tokens[1]);
+            switch (command){
+                case "Delete":
+                    for (int i = 0; i <numbers.size() ; i++) {
+                        if (numbers.get(i)==element){
+                            numbers.remove(i);
+                        }else{
+                            continue;
+                        }
+                    }
+                    break;
+                case "Insert":
+                    int index = Integer.parseInt(tokens[2]);
+                    numbers.add(index,element);
+                    break;
+            }
+            input=scanner.nextLine();
+        }
+
+        for (int i = 0; i <numbers.size() ; i++) {
+            System.out.print(numbers.get(i) + " ");
+
+        }
+    }
 }
+
