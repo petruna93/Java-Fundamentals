cb379366710a1e1c0ce295e7bfe7ce996d0690ed
Petya Zhekova <71344477+petruna93@users.noreply.github.com>
2021-06-21 22:41:57 +0300

    Songs

---
+++ ObjectsAndClassesLab/Songs/Song.java
@@ -0,0 +1,37 @@
+package ObjectsAndClassesLab;
+
+public class Song {
+    String typeList;
+    String name;
+    String time;
+
+    public Song(String typeList, String name, String time) {
+        this.typeList = typeList;
+        this.name = name;
+        this.time = time;
+    }
+
+    public void setTypeList(String typeList) {
+        this.typeList = typeList;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTypeList() {
+        return typeList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
